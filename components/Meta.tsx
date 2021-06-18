95b33feed156bcc62cd3802f7d1488485a1b7c96
ihadabs <hadi.albinsaad@gmail.com>
2021-06-19 00:53:59 +0300

    refactor meta comp.

--- components/Meta.tsx
+++ components/Meta.tsx
@@ -1,14 +1,20 @@
 import { Head } from 'next/document';
 
-const Meta = ({ title, keywords, description }: { title: string, description: string, keywords: string[] }) => (
-	<Head>
+interface IMeta {
+	title: string;
+	description: string;
+	keywords: string[];
+}
+
+export default function Meta({ title, keywords, description }: IMeta) {
+	return <Head>
 		<meta name="viewport" content="initial-scale=1.0, width=device-width"/>
 		<meta name="keywords" content={keywords.join(' ')}/>
 		<meta name="description" content={description}/>
 		<link rel="icon" href="/favicon.ico"/>
 		<title>{title}</title>
 		<meta/>
-	</Head>
-);
+	</Head>;
+}
 
-export default Meta;
+
