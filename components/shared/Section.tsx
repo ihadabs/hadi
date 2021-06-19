38c882e1dbc5400a10b239e0c801cdf7e4c64c00
ihadabs <hadi.albinsaad@gmail.com>
2021-06-20 01:37:51 +0300

    refactor home page + cretae new section comp.

--- components/shared/Section.tsx
+++ components/shared/Section.tsx
@@ -1,6 +1,22 @@
-export default function Section() {
+import { CSSProperties, PropsWithChildren, ReactNode } from 'react';
+
+export interface ISection {
+	title: string;
+	subtitle?: string | ReactNode;
+}
+
+const titleStyle: CSSProperties = {
+	fontSize: '1.5rem',
+	lineHeight: '1.5'
+};
+
+export default function Section({ title, subtitle, children }: PropsWithChildren<ISection>) {
 	return (
 		<div>
-
-		</div>);
+			<h3 style={titleStyle}>{title}</h3>
+			<p>{subtitle}</p>
+			<div style={{ height: '0.5rem', }}/>
+			{children}
+		</div>
+	);
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,54 +3,45 @@ import NavItem from '../components/core/NavItem';
 import { navItems } from '../components/core/Nav';
 import Accounts from '../components/shared/Accounts';
 import styles from '../styles/Home.module.css';
+import Section from '../components/shared/Section';
 
 export default function Home() {
 	return (
 		<div className="container">
-			<Meta title="Hadi" keywords={[]} description=""/>
+			<Meta title="Hadi" keywords={['hadi', 'albinsaad', 'هادي', 'البن سعد']} description=""/>
 
-			<div style={{ height: '1rem', }}/>
+			
 			<h2 className={styles.title}>حيا الله من جانا 👋🏼</h2>
-			<div style={{ height: '3rem', }}/>
 
-			<h3 className={styles.description}>خذ لك بيتين ع السريع</h3>
-			<div style={{ height: '0.5rem', }}/>
-			<p style={{ display: 'flex', flexWrap: 'wrap', gridGap: '1rem' }}>
+			<Section title="خذ لك بيتين ع السريع">
+				<div style={{ height: '0.5rem', }}/>
+				<p style={{ display: 'flex', flexWrap: 'wrap', gridGap: '1rem' }}>
 				<span className={styles.quote}>
 					دواؤك فيك وما تشعرُ
 				</span>
-				<span className={styles.quote}>
+					<span className={styles.quote}>
 					وداؤك مِنك وما تُبصرُ
 				</span>
-				<span className={styles.quote}>
+					<span className={styles.quote}>
 					وتحسب أنك جرمٌ صغيرٌ
 				</span>
-				<span className={styles.quote}>
+					<span className={styles.quote}>
 					وفيك انطوى العالمُ الأكبرُ
 				</span>
-			</p>
+				</p>
+			</Section>
 
-			<div style={{ height: '3rem', }}/>
-			<h3 className={styles.description}>عندك وظايف؟</h3>
-			<p> ابد اختر وسيلة التواصل اللي تناسبك 👇</p>
-			<div style={{ height: '0.5rem', }}/>
-			<Accounts/>
+			<Section title="عندك وظايف؟" subtitle="ابد اختر وسيلة التواصل اللي تناسبك 👇">
+				<Accounts/>
+			</Section>
 
+			<Section title="ودك بمشورة تقنية وأنا وأخوك؟" subtitle="اضغط على زر استشارة تقنية وخل نشوف كيف نقدر نفيدك.">
+				<NavItem item={navItems[1]}/>
+			</Section>
 
-			<div style={{ height: '3rem', }}/>
-			<h3 className={styles.description}>ودك بمشورة تقنية وأنا وأخوك؟</h3>
-			<p>اضغط على زر استشارة تقنية وخل نشوف كيف نقدر نفيدك.</p>
-			<div style={{ height: '0.5rem', }}/>
-			<NavItem item={navItems[1]}/>
-
-
-			<div style={{ height: '3rem', }}/>
-			<h3 className={styles.description}>ودك تعرف عني اكثر؟</h3>
-			<p>اضغط على زر من أنا، وإذا ودك تعال تويتر عرفنا عليك.</p>
-			<div style={{ height: '0.5rem', }}/>
-			<NavItem item={navItems[0]}/>
-
-			<div style={{ height: '3rem', }}/>
+			<Section title="ودك تعرف عني اكثر؟" subtitle="اضغط على زر من أنا، وإذا ودك تعال تويتر عرفنا عليك.">
+				<NavItem item={navItems[0]}/>
+			</Section>
 		</div>
 	);
 }
