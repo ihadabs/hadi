5ed37bc1d6d5537f2a1237a79aa1f0e6222bbe55
ihadabs <hadi.albinsaad@gmail.com>
2021-06-21 00:55:32 +0300

    custom 500 page

---
+++ pages/500.tsx
@@ -0,0 +1,8 @@
+import Page from '../components/shared/Page';
+
+export default function Custom500() {
+	return (<Page meta={{ title: 'Hadi | Page Not Found' }}
+				  title="حدث خطأ أثناء جلب الصفحة 🪢">
+		الرجاء تحديث الصفحة.
+	</Page>);
+}
