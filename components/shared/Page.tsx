113123eb1e1d4f24cc120ab8e7452792409a9149
ihadabs <hadi.albinsaad@gmail.com>
2021-06-20 03:49:57 +0300

    add calendly inline embed

--- components/shared/Page.tsx
+++ components/shared/Page.tsx
@@ -4,15 +4,19 @@ import Meta, { IMeta } from '../core/Meta';
 export interface IPage {
 	meta: IMeta;
 	title: string;
+	subtitle?: string;
 }
 
-export default function Page({ meta, title, children }: PropsWithChildren<IPage>) {
+export default function Page({ meta, title, subtitle, children }: PropsWithChildren<IPage>) {
 	return (
 		<div className="pageContainer">
 			<Meta title={meta.title} keywords={meta.keywords} description={meta.description}/>
 
-			<h2 className="pageTitle">{title}</h2>
-			
+			<div>
+				<h2 className="pageTitle">{title}</h2>
+				<p>{subtitle}</p>
+			</div>
+
 			{children}
 		</div>
 	);
--- pages/consulting.tsx
+++ pages/consulting.tsx
@@ -1,25 +1,32 @@
 import { IMeta } from '../components/core/Meta';
 import Page from '../components/shared/Page';
+import Section from '../components/shared/Section';
 
 const consultingMeta: IMeta = {
 	title: 'هادي | استشارة تقنية  📡',
 	description: 'ودك تبدأ مشروع تقني ومو عارف وش تسوي؟ خذ لك استشارة تقنية من هادي 🚀! ',
 	keywords: ['Hadi', 'هادي', 'Tech', 'Consulting', 'استشارة', 'تقنية']
-
 };
+
+const subtitle = 'شغال على مشروع وفيه جانب تقني ودك تمسكه صح؟ عندك فكرة تطبيق او موقع وودك تبدأ فيها؟ طالب وودك تطور نفسك تقنيا؟ وصلت خير 😁. ببنجلس وبنناقش الوضع وإن شاء الله نطلع بحلول فعّالة لحالتك 🤝. ';
 export default function Consulting() {
 	return (
-		<Page meta={consultingMeta} title="استشارة تقنية 📡">
-			عندك مشروع مو عارف تبداه؟
-			<br/>
-			انت مو تقني وودك تبدأ في التقنية؟
-			<br/>
-			انت تقني وودك تطور نفسك؟
-			<br/>
-			تبي نصيحة في اي شيء له دخل في بناء المشاريع التقنية؟
-			<br/>
-			<br/>
-			ب٢٠٠ ريال اعطيك احسن استشارة تقنية ✅
+		<Page meta={consultingMeta} title="استشارة تقنية 📡" subtitle={subtitle}>
+			<Section title="المتوقع من الاستشارة">
+			</Section>
+
+			<Section title="ملاحظات هامة">
+			</Section>
+
+			<Section title="معلومات الاستشارة">
+			</Section>
+
+			<Section title="للحجز 📅" subtitle="اختر اليوم والوقت اللي يناسبك وبإذن الله برد عليك في اقرب وقت 🤝.">
+
+				<div className="calendly-inline-widget" data-url="https://calendly.com/hadialbinsaad?hide_gdpr_banner=1"
+					 style={{ minWidth: '320px', height: '630px' }}/>
+				<script type="text/javascript" src="https://assets.calendly.com/assets/external/widget.js" async/>
+			</Section>
 		</Page>
 	);
 }
