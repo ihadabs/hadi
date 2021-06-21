a5d843f0693566451576382bac4857413aaf49c1
ihadabs <hadi.albinsaad@gmail.com>
2021-06-22 02:46:53 +0300

    localize structured data

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,5 +1,50 @@
 import Document, { DocumentContext, Head, Html, Main, NextScript } from 'next/document';
 
+const enStructuredData = {
+	'@context': 'https://schema.org/',
+	'@type': 'Person',
+	'givenName': 'Hadi',
+	'familyName': 'Albinsaad',
+	'name': 'Hadi Albinsaad',
+	'additionalName': 'Hadi Albinsaad',
+	'affiliation': {
+		'@context': 'https://schema.org',
+		'@type': 'Organization',
+		'url': 'https://axenda.io',
+		'name': 'Axenda',
+		'legalName': 'Axenda',
+		'email': 'info@axenda.io',
+		'logo': 'https://axenda.io/assets/logos/axenda-logo.svg',
+		'foundingDate': '2020-3-17',
+		'founder': 'Hadi Albinsaad'
+	},
+	'jobTitle': 'Software Engineer',
+	'image': 'https://hadialbinsaad.com/avatar',
+	'email': 'hadi.albinsaad@gmail.com',
+};
+const arStructuredData = {
+	'@context': 'https://schema.org/',
+	'@type': 'Person',
+	'givenName': 'هادي',
+	'familyName': 'البن سعد',
+	'name': 'هادي البن سعد',
+	'additionalName': 'هادي البن سعد',
+	'affiliation': {
+		'@context': 'https://schema.org',
+		'@type': 'Organization',
+		'url': 'https://axenda.io',
+		'name': 'اكسندا',
+		'legalName': 'اكسندا',
+		'email': 'info@axenda.io',
+		'logo': 'https://axenda.io/assets/logos/axenda-logo.svg',
+		'foundingDate': '2020-3-17',
+		'founder': 'هادي البن سعد'
+	},
+	'jobTitle': 'مهندس برمجيات',
+	'image': 'https://hadialbinsaad.com/avatar',
+	'email': 'hadi.albinsaad@gmail.com',
+};
+
 class MyDocument extends Document {
 	static async getInitialProps(ctx: DocumentContext) {
 		return await Document.getInitialProps(ctx);
@@ -8,9 +53,8 @@ class MyDocument extends Document {
 	render() {
 		const { locale } = this.props.__NEXT_DATA__;
 		const dir = locale === 'ar' ? 'rtl' : 'ltr';
+		const structuredData = locale === 'ar' ? arStructuredData : enStructuredData;
 
-		// @ts-ignore
-		// @ts-ignore
 		return (
 			<Html lang={locale} dir={dir}>
 				<Head>
@@ -35,30 +79,7 @@ class MyDocument extends Document {
 					{/* Structured Data */}
 					<script
 						type="application/ld+json"
-						dangerouslySetInnerHTML={{
-							__html: JSON.stringify({
-								'@context': 'https://schema.org/',
-								'@type': 'Person',
-								'givenName': 'Hadi',
-								'familyName': 'Albinsaad',
-								'name': 'Hadi Albinsaad',
-								'additionalName': 'Hadi Albinsaad',
-								'affiliation': {
-									'@context': 'https://schema.org',
-									'@type': 'Organization',
-									'url': 'https://axenda.io',
-									'name': 'Axenda',
-									'legalName': 'Axenda',
-									'email': 'info@axenda.io',
-									'logo': 'https://axenda.io/assets/logos/axenda-logo.svg',
-									'foundingDate': '2020-3-17',
-									'founder': 'Hadi Albinsaad'
-								},
-								'jobTitle': 'Software Engineer',
-								'image': 'https://hadialbinsaad.com/avatar',
-								'email': 'hadi.albinsaad@gmail.com',
-							})
-						}}
+						dangerouslySetInnerHTML={{ __html: JSON.stringify(structuredData) }}
 					/>
 				</Head>
 				<body>
