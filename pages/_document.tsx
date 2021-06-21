77c4f22b6f6078491a7145e461afa3684ab9917b
ihadabs <hadi.albinsaad@gmail.com>
2021-06-21 06:12:02 +0300

    remove unused font weights

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -11,10 +11,10 @@ class MyDocument extends Document {
 		return (
 			<Html lang={locale} dir={dir}>
 				<Head>
-					<link rel="dns-prefetch" href="//fonts.googleapis.com" crossOrigin="true"/>
-					<link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true"/>
+					{/*<link rel="dns-prefetch" href="//fonts.googleapis.com" crossOrigin="true"/>*/}
+					{/*<link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true"/>*/}
 					<link
-						href="https://fonts.googleapis.com/css2?family=Aref+Ruqaa:wght@400;700&family=Comfortaa:wght@300;400;500;600;700&family=Tajawal:wght@200;300;400;500;700;800;900&display=swap"
+						href="https://fonts.googleapis.com/css2?family=Comfortaa:wght@400;500;700&family=Tajawal:wght@400;500;700;&display=swap"
 						rel="stylesheet"/>
 				</Head>
 				<body>
