498b1cd6e5e2ba1c6210cebad30fdf0958dcc35b
ihadabs <hadi.albinsaad@gmail.com>
2022-04-19 05:09:48 +0300

    Update domain

--- next-sitemap.js
+++ next-sitemap.js
@@ -1,15 +1,15 @@
 module.exports = {
-	siteUrl: process.env.SITE_URL || 'https://hadialbinsaad.com',
+	siteUrl: process.env.SITE_URL || 'https://hadi.gg',
 	generateRobotsTxt: true,
 	exclude: ['/ar*', '/en*', '/500*', '/404*'],
 	alternateRefs: [
 		{
-			href: 'https://hadialbinsaad.com/ar',
+			href: 'https://hadi.gg/ar',
 			hreflang: 'ar',
 		},
 		{
-			href: 'https://hadialbinsaad.com/en',
+			href: 'https://hadi.gg/en',
 			hreflang: 'en',
 		},
-	]
+	],
 };
